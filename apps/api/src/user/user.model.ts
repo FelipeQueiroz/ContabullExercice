22a2e435811ed7ee7532acb6c993aa14a6c5ad4b
FelipeQueiroz <felipeqmcorreia@gmail.com>
2022-10-30 10:35:26 -0300

    feat: adding user routes and config

---
+++ apps/api/src/user/user.model.ts
@@ -0,0 +1,31 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class UserModel {
+  @Field()
+  id: number;
+
+  @Field()
+  email: string;
+
+  @Field()
+  password: string;
+
+  @Field({ nullable: true })
+  favPet?: string;
+
+  @Field({ nullable: true })
+  name?: string;
+
+  @Field({ nullable: true })
+  lastName?: string;
+
+  @Field({ nullable: true })
+  city?: string;
+
+  @Field({ nullable: true })
+  married?: boolean;
+
+  @Field({ nullable: true })
+  birthDay?: Date;
+}
---
+++ apps/api/src/user/user.resolver.ts
@@ -0,0 +1,68 @@
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
+import { UserModel } from './user.model';
+import { UserService } from './user.service';
+
+@Resolver(() => UserModel)
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+  @Mutation((returns) => UserModel)
+  async createUser(
+    @Args('password') password: string,
+    @Args('email') email: string,
+    @Args('name', { nullable: true }) name: string,
+    @Args('birthDay', { nullable: true }) birthDay: string,
+    @Args('city', { nullable: true }) city: string,
+    @Args('favPet', { nullable: true }) favPet: string,
+    @Args('favTech', { nullable: true }) favTech: string,
+    @Args('married', { nullable: true }) married: boolean,
+  ): Promise<UserModel> {
+    return this.userService.createUser({
+      email,
+      password,
+      name,
+      birthDay,
+      city,
+      favPet,
+      favTech,
+      married,
+    });
+  }
+
+  @Query((returns) => UserModel)
+  @UseGuards(GqlAuthGuard)
+  async me(@Args('email') email: string): Promise<UserModel> {
+    return this.userService.user({ email: email });
+  }
+
+  @Mutation((returns) => UserModel)
+  @UseGuards(GqlAuthGuard)
+  async updateUser(
+    @Args('password', { nullable: true }) password: string,
+    @Args('oldEmail') oldEmail: string,
+    @Args('newEmail', { nullable: true }) newEmail: string,
+    @Args('name', { nullable: true }) name: string,
+    @Args('birthDay', { nullable: true }) birthDay: string,
+    @Args('city', { nullable: true }) city: string,
+    @Args('favPet', { nullable: true }) favPet: string,
+    @Args('favTech', { nullable: true }) favTech: string,
+    @Args('married', { nullable: true }) married: boolean,
+  ): Promise<UserModel> {
+    return this.userService.updateUser({
+      data: {
+        email: newEmail,
+        password,
+        name,
+        birthDay,
+        city,
+        favPet,
+        favTech,
+        married,
+      },
+      where: {
+        email: oldEmail,
+      },
+    });
+  }
+}
