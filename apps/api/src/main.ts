379fd7ae56cbdf4e10cf51e52936a217dccf637c
FelipeQueiroz <felipeqmcorreia@gmail.com>
2022-10-31 20:55:32 -0300

    feat: adding graphqlschema builder to main api file

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  GraphQLSchemaBuilderModule,
+  GraphQLSchemaFactory,
+} from '@nestjs/graphql';
+import { printSchema } from 'graphql';
 import { AppModule } from './app.module';
+import { AuthResolver } from './auth/auth.resolver';
+import { UserResolver } from './user/user.resolver';
+
+async function generateSchema() {
+  const app = await NestFactory.create(GraphQLSchemaBuilderModule);
+  await app.init();
+
+  const gqlSchemaFactory = app.get(GraphQLSchemaFactory);
+  const schema = await gqlSchemaFactory.create([UserResolver, AuthResolver], {
+    skipCheck: true,
+    orphanedTypes: [],
+  });
+  console.log(printSchema(schema));
+}
 
 async function bootstrap() {
+  generateSchema();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
