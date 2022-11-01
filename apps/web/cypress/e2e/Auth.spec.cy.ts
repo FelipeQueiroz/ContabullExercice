a24a84bee2cd114770bd42bf07cb744c37e4e072
FelipeQueiroz <felipeqmcorreia@gmail.com>
2022-11-01 01:15:01 -0300

    feat: adding auth tests

---
+++ apps/web/cypress/e2e/Auth.spec.cy.ts
@@ -0,0 +1,45 @@
+const userExample = {
+  email: `johndoe${Math.floor(Math.random() * 100)}@gmail.com`,
+  password: '123'
+}
+
+describe('Signup spec', () => {
+  it('should create an account on system', () => {
+    cy.visit('http://localhost:5173/signup')
+    cy.get('#email').type(userExample.email)
+    cy.get('#password').type(userExample.password)
+    cy.get('#confirmPassword').type(userExample.password)
+    cy.get('#submitButton').click()
+
+    expect(cy.url().should('eq', 'http://localhost:5173/'))
+  })
+})
+
+describe('Login Spec', () => {
+  it('should login on system', () => {
+    cy.visit('http://localhost:5173/')
+    cy.get('#email').type('fq@gmail.com')
+    cy.get('#password').type('123')
+    cy.get('#submitButton').click()
+
+    expect(cy.url().should('eq', 'http://localhost:5173/dashboard'))
+  })
+})
+
+describe('Edit Profile Spec', () => {
+  it('should login on system', () => {
+    it('should login and edit profile by chaging name', () => {
+      cy.visit('http://localhost:5173/')
+      cy.get('#email').type('fq@gmail.com')
+      cy.get('#password').type('123')
+      cy.get('#submitButton').click()
+      expect(cy.url().should('eq', 'http://localhost:5173/dashboard'))
+      cy.get('#editProfileLink').click()
+      expect(cy.url().should('eq', 'http://localhost:5173/dashboard/update'))
+      cy.get('#name').type('Felipe Queiroz')
+      cy.get('#submitButton').click()
+      expect(cy.url().should('eq', 'http://localhost:5173/dashboard'))
+      cy.get('h1').contains('Welcome, Felipe Queiroz')
+    })
+  })
+})
